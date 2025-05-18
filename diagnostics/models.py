5d1c825288aef91e335ce138ef7c1dfa58e1963d
Maslov Vladislav <maslov.vladislav.evgenevich@gamil.com>
2025-05-18 23:48:45 +0300

    документация завершённая

--- diagnostics/models.py
+++ diagnostics/models.py
@@ -1,33 +1,3 @@
-'''
-
-from django.db import models
-from django.conf import settings
-
-class DiagnosticReport(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name='diagnostic_reports',
-        verbose_name='Пользователь'
-    )
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-    description = models.TextField(verbose_name='Описание неисправностей')
-    recommendations = models.TextField(verbose_name='Рекомендуемые работы')
-    required_parts = models.ManyToManyField(
-        'details.Part',
-        blank=True,
-        related_name='diagnostic_reports',
-        verbose_name='Необходимые запчасти'
-    )
-
-    class Meta:
-        verbose_name = 'Отчёт о диагностике'
-        verbose_name_plural = 'Отчёты о диагностике'
-        ordering = ['-created_at']
-
-    def __str__(self):
-        return f"Отчёт от {self.created_at.strftime('%d.%m.%Y')} для {self.user.email}"
-'''
 # diagnostics/models.py
 from django.db import models
 from authorization.models import CustomUser
